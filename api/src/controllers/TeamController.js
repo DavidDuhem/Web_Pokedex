f03cff6a27c48329a1efd10c8bd070e002c193e2
DavidDuhem <synlike@gmail.com>
2025-06-29 18:15:57 +0200

    Get, Add and Remove pokemon from team routes and controllers

--- api/src/controllers/TeamController.js
+++ api/src/controllers/TeamController.js
@@ -1,10 +1,67 @@
-import { Team, sequelize } from "../models/index.js";
+import { Team, Pokemon, sequelize } from "../models/index.js";
 import BaseController from "./BaseController.js";
 import teamchema from "../schemas/team.schema.js";
 
-class TeamController extends BaseController {
+export default class TeamController extends BaseController {
   constructor() {
     super(Team, teamchema);
   }
+
+  async getTeamPokemons(req, res) {
+    const teamId = parseInt(req.params.id);
+
+    try {
+      const teamWithPokemons = await Team.findByPk(teamId, {
+        include: {
+          model: Pokemon,
+          as: "pokemons",
+          through: { attributes: [] },
+        },
+      });
+
+      if (!teamWithPokemons)
+        return res.status(404).json({ error: "Team and Pokemons not found" });
+      res.json(teamWithPokemons);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  }
+
+  async addPokemonToTeam(req, res) {
+    const teamId = parseInt(req.params.id);
+    const { pokemonId } = req.body;
+
+    try {
+      const team = await Team.findByPk(teamId);
+      if (!team) return res.status(404).json({ error: "Team not found" });
+
+      const pokemon = await Pokemon.findByPk(pokemonId);
+      if (!pokemon) return res.status(404).json({ error: "Pokemon not found" });
+
+      await team.addPokemon(pokemon);
+
+      res.json({ message: "Pokemon added to team successfully" });
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  }
+
+  async removePokemonFromTeam(req, res) {
+    const teamId = parseInt(req.params.id);
+    const pokemonId = parseInt(req.params.pokemonId);
+
+    try {
+      const team = await Team.findByPk(teamId);
+      if (!team) return res.status(404).json({ error: "Team not found" });
+
+      const pokemon = await Pokemon.findByPk(pokemonId);
+      if (!pokemon) return res.status(404).json({ error: "Pokemon not found" });
+
+      await team.removePokemon(pokemon);
+
+      res.json({ message: "Pokemon removed from team successfully" });
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  }
 }
-export default new TeamController();
--- api/src/router.js
+++ api/src/router.js
@@ -2,9 +2,11 @@ import { Router } from "express";
 import { controllerWrapper as cw } from "./utils/controllerWrapper.js";
 import pokemonController from "./controllers/PokemonController.js";
 import typeController from "./controllers/TypeController.js";
-import teamController from "./controllers/TeamController.js";
+import TeamController from "./controllers/TeamController.js";
 
 export const router = Router();
+
+const teamController = new TeamController();
 
 // == Routes des Pokemons ==
 
@@ -27,6 +29,17 @@ router.get(
   "/types/:id",
   cw((req, res, next) => typeController.getById(req, res, next))
 );
+
+// == Routes des Teams ==
+
+router.get(
+  "/teams",
+  cw((req, res) => teamController.getAll(req, res))
+);
+router.get(
+  "/teams/:id",
+  cw((req, res, next) => teamController.getById(req, res, next))
+);
 router.post(
   "/teams",
   cw((req, res, next) => teamController.create(req, res, next))
@@ -39,16 +52,17 @@ router.delete(
   "/teams/:id",
   cw((req, res, next) => teamController.delete(req, res, next))
 );
-
-// == Routes des Teams ==
-
 router.get(
-  "/teams",
-  cw((req, res) => teamController.getAll(req, res))
+  "/teams/:id/pokemons",
+  cw((req, res, next) => teamController.getTeamPokemons(req, res, next))
 );
-router.get(
-  "/teams/:id",
-  cw((req, res, next) => teamController.getById(req, res, next))
+router.post(
+  "/teams/:id/pokemons",
+  cw((req, res, next) => teamController.addPokemonToTeam(req, res, next))
+);
+router.delete(
+  "/teams/:id/pokemons/:pokemonId",
+  cw((req, res, next) => teamController.removePokemonFromTeam(req, res, next))
 );
 
 // Middleware 404
