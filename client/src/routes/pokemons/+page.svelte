5f96621b37ee166e39d743693df18e21c99b4d02
DavidDuhem <synlike@gmail.com>
2025-06-30 14:49:59 +0200

    Prepared front for pokemon details

--- client/src/routes/pokemons/+page.svelte
+++ client/src/routes/pokemons/+page.svelte
@@ -4,14 +4,30 @@
   export let data;
 </script>
 
-{#if data.error}
-  <p>Erreur : {data.error}</p>
-{:else if !data.pokemons || data.pokemons.length === 0}
-  <p>Aucun pokémon trouvé.</p>
-{:else}
-  <ul>
-    {#each data.pokemons as pokemon}
-      <li><a href={`/pokemons/${pokemon.id}`}>{pokemon.name}</a></li>
-    {/each}
-  </ul>
-{/if}
+<div class="max-w-4xl mx-auto mt-8 px-4">
+  <h1 class="text-3xl font-bold text-red-600 mb-4">Liste des Pokémons</h1>
+  {#if data.error}
+    <p>Erreur : {data.error}</p>
+  {:else if !data.pokemons || data.pokemons.length === 0}
+    <p>Aucun pokémon trouvé.</p>
+  {:else}
+    <div
+      class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 mt-10 mb-20"
+    >
+      {#each data.pokemons as pokemon}
+        <a
+          href={`/pokemons/${pokemon.id}`}
+          class="border rounded p-2 flex flex-col items-center hover:shadow-lg transition cursor-pointer hover:bg-gray-200"
+        >
+          <span class="text-sm text-gray-500">ID: {pokemon.id}</span>
+          <img
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
+            alt={pokemon.name}
+            class="w-20 h-20 object-contain mb-2"
+          />
+          <span class="text-red-600 font-semibold">{pokemon.name}</span>
+        </a>
+      {/each}
+    </div>
+  {/if}
+</div>
--- client/src/routes/pokemons/[id]/+page.svelte
+++ client/src/routes/pokemons/[id]/+page.svelte
@@ -1,5 +1,53 @@
 <script>
-  /** @type {{ data: import('./$types').PageData }} */
-
+  /** @type {{ pokemon: any }} */
   export let data;
+  const pokemon = data.pokemon;
 </script>
+
+<div class="max-w-4xl mx-auto p-6">
+  <h1 class="text-3xl font-bold mb-4 capitalize">
+    {pokemon.name} (#{pokemon.id})
+  </h1>
+
+  <img
+    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
+    alt={pokemon.name}
+    class="w-64 h-64 mx-auto mb-6"
+  />
+
+  <div class="flex flex-wrap gap-2 justify-center mb-6">
+    {#each pokemon.types as type}
+      <span class="bg-gray-200 rounded-full px-3 py-1 text-sm capitalize">
+        {type.type.name}
+      </span>
+    {/each}
+  </div>
+
+  <h2 class="text-xl font-semibold mb-2">Stats</h2>
+  <ul class="mb-6">
+    <li>
+      <span class="capitalize">HP : </span>
+      {pokemon.hp}
+    </li>
+    <li>
+      <span class="capitalize">ATK : </span>
+      {pokemon.atk}
+    </li>
+    <li>
+      <span class="capitalize">DEF : </span>
+      {pokemon.def}
+    </li>
+    <li>
+      <span class="capitalize">ATK_SPE : </span>
+      {pokemon.atk_spe}
+    </li>
+    <li>
+      <span class="capitalize">DEF_SPE : </span>
+      {pokemon.def_spe}
+    </li>
+    <li>
+      <span class="capitalize">SPEED : </span>
+      {pokemon.speed}
+    </li>
+  </ul>
+</div>
