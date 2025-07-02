f8a7e5b2ea8f4435843a2148a96c0c3cde340e5b
DavidDuhem <synlike@gmail.com>
2025-07-02 12:56:15 +0200

    Init config-swagger and test on one route

--- api/src/routes/auth.routes.js
+++ api/src/routes/auth.routes.js
@@ -6,6 +6,38 @@ const router = Router();
 
 const authController = new AuthController();
 
+/**
+ * @swagger
+ * /api/auth/register:
+ *   post:
+ *     summary: Enregistrer un nouvel utilisateur
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - password
+ *               - username
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 example: "user@example.com"
+ *               password:
+ *                 type: string
+ *                 format: password
+ *                 example: "Password"
+ *               username:
+ *                 type: string
+ *                 example: "Username"
+ *     responses:
+ *       201:
+ *         description: Utilisateur créé
+ */
+
 router.post(
   "/register",
   cw((req, res, next) => authController.register(req, res, next))
