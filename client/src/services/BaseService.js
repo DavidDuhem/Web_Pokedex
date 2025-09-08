bf67007d2dab66b224e82e7debd4d6e7c90be6d3
David <synlike@gmail.com>
2025-09-08 16:32:48 +0200

    Rename API_BASE_URL to VITE_API_BASE_URL

--- client/src/services/BaseService.js
+++ client/src/services/BaseService.js
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-    import.meta.env.API_BASE_URL || "http://localhost:3000/api";
+    import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api";
 
 import { token } from "../stores/auth.js";
 import { get } from "svelte/store";
